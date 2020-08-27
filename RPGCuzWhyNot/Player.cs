195b97661a2181b0054d4e8e4ccb30b4b24a8ff2
StaterZ <melvin@ringheim.se>
2020-08-27 21:48:25 +0200

    split world to own file

--- RPGCuzWhyNot/Player.cs
+++ RPGCuzWhyNot/Player.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace RPGCuzWhyNot {
 	public class Player : Character {
@@ -73,33 +72,6 @@ namespace RPGCuzWhyNot {
 			}
 
 			return false;
-		}
-	}
-
-	public class World {
-		private readonly List<Location> locations = new List<Location>();
-
-		/// <summary>
-        /// finds a location by its name. if not found it returns null
-        /// </summary>
-        /// <param name="callName">the name of the location</param>
-        /// <returns></returns>
-		public Location GetLocationByCallName(string callName) {
-			foreach (Location location in locations) {
-				if (location.callName == callName) {
-					return location;
-				}
-			}
-
-			return null;
-		}
-
-		public void RegisterNewLocation(Location location) {
-			locations.Add(location);
-		}
-
-		public void MakePath(Location a, Location b) {
-			
 		}
 	}
 }
---
+++ RPGCuzWhyNot/World.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace RPGCuzWhyNot {
+	public class World {
+		private readonly List<Location> locations = new List<Location>();
+
+		/// <summary>
+		/// finds a location by its name. if not found it returns null
+		/// </summary>
+		/// <param name="callName">the name of the location</param>
+		/// <returns></returns>
+		public Location GetLocationByCallName(string callName) {
+			foreach (Location location in locations) {
+				if (location.callName == callName) {
+					return location;
+				}
+			}
+
+			return null;
+		}
+
+		public void RegisterNewLocation(Location location) {
+			locations.Add(location);
+		}
+
+		public void MakePath(Location a, Location b) {
+			
+		}
+	}
+}
