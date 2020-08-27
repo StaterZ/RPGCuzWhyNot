9ba401c43efdcfa31b9c7cc48177995519aed1c9
smeas <smeas@users.noreply.github.com>
2020-08-27 21:00:33 +0200

    add health and damage

--- RPGCuzWhyNot/Character.cs
+++ RPGCuzWhyNot/Character.cs
@@ -1,6 +1,31 @@
-﻿namespace RPGCuzWhyNot {
+﻿using System;
+
+namespace RPGCuzWhyNot {
 	public class Character {
 		public string name;
 		public Gender gender;
+		public int health;
+
+		public void Attack(Character target) {
+			int damage = CalculateDamage(target);
+			target.TakeDamage(damage, target);
+		}
+
+		public virtual void TakeDamage(int damage, Character source) {
+			damage = Math.Min(health, damage);
+			health -= damage;
+
+			Console.WriteLine($"{source.name} hit {name} for {damage} damage.");
+
+			if (health <= 0) {
+				Console.WriteLine($"{name} got killed by {source.name}.");
+				// TODO: Die
+			}
+		}
+
+		protected virtual int CalculateDamage(Character target) {
+			// TODO
+			return 2;
+		}
 	}
 }
--- RPGCuzWhyNot/Player.cs
+++ RPGCuzWhyNot/Player.cs
@@ -1,5 +1,19 @@
-﻿namespace RPGCuzWhyNot {
-	public class Player : Character {
+﻿using System;
 
+namespace RPGCuzWhyNot {
+	public class Player : Character {
+		public int health;
+
+		public override void TakeDamage(int damage, Character source) {
+			damage = Math.Min(health, damage);
+			health -= damage;
+
+			Console.WriteLine($"{source.name} hit you for {damage} damage.");
+
+			if (health <= 0) {
+				Console.WriteLine($"{source.name} killed you.");
+				// TODO: Die
+			}
+		}
 	}
 }
