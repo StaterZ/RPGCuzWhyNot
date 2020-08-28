b21d507624acc37446939d5284aed0df028046c0
StaterZ <melvin@ringheim.se>
2020-08-28 18:54:54 +0200

    remove copyright

--- RPGCuzWhyNot/HealthSystem/IAlignment.cs
+++ RPGCuzWhyNot/HealthSystem/IAlignment.cs
@@ -1,11 +1,3 @@
-/*--
-Copyright (C) 2020 Melvin Ringheim - All Rights Reserved
-This file falls under the default laws of exclusive copyright.
-Nobody can copy, distribute, or modify this code without explicit permission from the owner.
---*/
-
-
-
 namespace StaterZ.Core.HealthSystem {
 	public abstract class Alignment {
 		public abstract bool CanHarm(Alignment other);
