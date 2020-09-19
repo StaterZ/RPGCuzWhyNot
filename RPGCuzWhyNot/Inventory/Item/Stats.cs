1ff6c3ff107413508b0f203dd4a7c18ef9fae162
StaterZ <melvin@ringheim.se>
2020-09-19 20:02:40 +0200

    added operators to stats

--- RPGCuzWhyNot/Inventory/Item/Stats.cs
+++ RPGCuzWhyNot/Inventory/Item/Stats.cs
@@ -24,5 +24,17 @@ namespace RPGCuzWhyNot.Inventory.Item {
 			Accuracy = accuracy;
 			Fortitude = fortitude;
 		}
+
+		public static Stats operator +(Stats a, Stats b) {
+			return new Stats(a.Speed + b.Speed, a.Strength + b.Strength, a.Accuracy + b.Accuracy, a.Fortitude + b.Fortitude);
+		}
+
+		public static Stats operator -(Stats a, Stats b) {
+			return new Stats(a.Speed - b.Speed, a.Strength - b.Strength, a.Accuracy - b.Accuracy, a.Fortitude - b.Fortitude);
+		}
+
+		public override string ToString() {
+			return $"SPD:{ConsoleUtils.FormatInt(Speed)}, STR:{ConsoleUtils.FormatInt(Strength)}, ACC:{ConsoleUtils.FormatInt(Accuracy)}, FRT:{ConsoleUtils.FormatInt(Fortitude)}";
+		}
 	}
 }
