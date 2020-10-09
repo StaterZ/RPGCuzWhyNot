c749e82dc6c2daca4cf35fd4e1ada7544191cb92
StaterZ <melvin@ringheim.se>
2020-10-09 02:49:07 +0200

    added mod function to extra maths

--- RPGCuzWhyNot/Utilities/ExtraMath.cs
+++ RPGCuzWhyNot/Utilities/ExtraMath.cs
@@ -5,5 +5,13 @@ namespace RPGCuzWhyNot.Utilities {
 		public static float Clamp(float value, float min, float max) {
 			return Math.Min(Math.Max(value, min), max);
 		}
+
+		public static int Mod(int a, int b) {
+			if (b == 0) {
+				return 0;
+			}
+
+			return a - b * (int)Math.Floor((float)a / b);
+		}
 	}
 }
