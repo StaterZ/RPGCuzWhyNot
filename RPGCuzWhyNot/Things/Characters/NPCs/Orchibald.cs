ad0d209ed9fdc8a7d956235cfbfe422008270870
StaterZ <melvin@ringheim.se>
2020-10-10 19:07:06 +0200

    added basic combat AI to Orchiblad

--- RPGCuzWhyNot/Things/Characters/NPCs/Orchibald.cs
+++ RPGCuzWhyNot/Things/Characters/NPCs/Orchibald.cs
@@ -1,6 +1,7 @@
 using RPGCuzWhyNot.Systems;
 using RPGCuzWhyNot.Systems.AttackSystem;
 using RPGCuzWhyNot.Systems.Data;
+using RPGCuzWhyNot.Systems.HealthSystem;
 using RPGCuzWhyNot.Things.Characters.Races.Humanoids;
 using RPGCuzWhyNot.Utilities;
 
@@ -11,6 +12,7 @@ namespace RPGCuzWhyNot.Things.Characters.NPCs
 		private const int voiceFrequency = 400;
 
 		public Orchibald() : base(new Dwarf(Humanoid.Gender.Male)) {
+			health = new Health(100);
 		}
 
 		public override void Converse(Character character, string response) {
@@ -23,7 +25,11 @@ namespace RPGCuzWhyNot.Things.Characters.NPCs
 		}
 
 		public override void DoTurn(Fight fight) {
-			throw new System.NotImplementedException();
+			foreach (Character combatant in fight.combatants) {
+				if (combatant == this) continue;
+
+				combatant.health.TakeDamage(20, this); //temp
+			}
 		}
 
 		public override bool WantsToHarm(Character character) {
