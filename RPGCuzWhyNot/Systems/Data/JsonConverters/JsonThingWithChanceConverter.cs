0c1e10e2139899684f325582c0241c90475999e3
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-10 17:52:21 +0200

    Add better error output from json converter

--- RPGCuzWhyNot/Systems/Data/JsonConverters/JsonThingWithChanceConverter.cs
+++ RPGCuzWhyNot/Systems/Data/JsonConverters/JsonThingWithChanceConverter.cs
@@ -15,12 +15,12 @@ namespace RPGCuzWhyNot.Systems.Data.JsonConverters {
 
 				reader.Read();
 				if (reader.TokenType != JsonToken.String)
-					throw new JsonException("Expected string.");
+					throw CreateException(reader, "Expected string.");
 				string itemId = (string)reader.Value;
 
 				reader.Read();
 				if (reader.TokenType != JsonToken.Integer && reader.TokenType != JsonToken.Float)
-					throw new JsonException("Expected number.");
+					throw CreateException(reader, "Expected number.");
 				object firstNumber = reader.Value;
 
 				reader.Read();
@@ -35,21 +35,29 @@ namespace RPGCuzWhyNot.Systems.Data.JsonConverters {
 
 					reader.Read();
 					if (reader.TokenType != JsonToken.EndArray)
-						throw new JsonException("Expected end of array.");
+						throw CreateException(reader, "Expected end of array.");
 
 					int minCount = Convert.ToInt32(firstNumber);
 					int maxCount = Convert.ToInt32(secondNumber);
 					return new ThingWithChance(itemId, minCount, maxCount);
 				}
 
-				throw new JsonException("Expected number.");
+				throw CreateException(reader, "Expected number.");
 			}
 
-			throw new JsonException("Expected string or array.");
+			throw CreateException(reader, "Expected string or array.");
 		}
 
 		public override void WriteJson(JsonWriter writer, ThingWithChance value, JsonSerializer serializer) {
 			throw new NotSupportedException();
+		}
+
+		private static JsonReaderException CreateException(JsonReader reader, string message) {
+			var lineInfo = (IJsonLineInfo)reader;
+
+			return new JsonReaderException(
+				$"{message} Path '{reader.Path}', line {lineInfo.LineNumber}, position {lineInfo.LinePosition}.",
+				reader.Path, lineInfo.LineNumber, lineInfo.LinePosition, null);
 		}
 	}
 }
