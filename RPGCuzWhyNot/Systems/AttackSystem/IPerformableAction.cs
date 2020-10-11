a59721b926302b236c76fb6be7634037445df5e8
StaterZ <melvin@ringheim.se>
2020-10-11 19:23:37 +0200

    remove un-needed properties from IPerformableAction

--- RPGCuzWhyNot/Systems/AttackSystem/IPerformableAction.cs
+++ RPGCuzWhyNot/Systems/AttackSystem/IPerformableAction.cs
@@ -2,10 +2,7 @@ using RPGCuzWhyNot.Things.Item;
 
 namespace RPGCuzWhyNot.Systems.AttackSystem {
 	public interface IPerformableAction {
-		bool HasTarget { get; }
 		string Name { get; }
-		string ListingName { get; }
-		string ExecuteDescription { get; }
 		Requirements Requirements { get; }
 
 		void Execute(TurnAction turnAction);
