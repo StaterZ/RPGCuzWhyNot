e6ab91c4dad4fb0426dd1840c5fc350cb5249092
Jonatan Johansson <smeas@users.noreply.github.com>
2020-10-11 15:52:53 +0200

    Fix mixed indentation in health system

--- RPGCuzWhyNot/Systems/HealthSystem/Health.cs
+++ RPGCuzWhyNot/Systems/HealthSystem/Health.cs
@@ -5,41 +5,41 @@ using RPGCuzWhyNot.Utilities;
 
 namespace RPGCuzWhyNot.Systems.HealthSystem {
 	public class Health {
-        public int maxHealth;
-        public IAlignment alignment;
-        public int CurrentHealth { get; private set; }
-        public float Percent => (float)CurrentHealth / maxHealth;
-        public bool IsDamaged => CurrentHealth < maxHealth;
-        public bool IsAlive => CurrentHealth > 0;
-        public bool IsAtMaxHealth => CurrentHealth >= maxHealth;
+		public int maxHealth;
+		public IAlignment alignment;
+		public int CurrentHealth { get; private set; }
+		public float Percent => (float)CurrentHealth / maxHealth;
+		public bool IsDamaged => CurrentHealth < maxHealth;
+		public bool IsAlive => CurrentHealth > 0;
+		public bool IsAtMaxHealth => CurrentHealth >= maxHealth;
 
-        public event Action<HealthChangeInfo> OnDeath; //when we run out of health
-        public event Action<HealthChangeInfo> OnChange; //if we in any way change the current health
-        public event Action<HealthChangeInfo> OnDamage; //if we explicity deal damage
-        public event Action<HealthChangeInfo> OnHeal; //if we explicity heal damage
-        public event Action<HealthChangeInfo> OnFullRecovery; //when we reach max health
+		public event Action<HealthChangeInfo> OnDeath; //when we run out of health
+		public event Action<HealthChangeInfo> OnChange; //if we in any way change the current health
+		public event Action<HealthChangeInfo> OnDamage; //if we explicity deal damage
+		public event Action<HealthChangeInfo> OnHeal; //if we explicity heal damage
+		public event Action<HealthChangeInfo> OnFullRecovery; //when we reach max health
 
-        public List<IHealthChangeModifier> armor = new List<IHealthChangeModifier>(); //damage modifiers
+		public List<IHealthChangeModifier> armor = new List<IHealthChangeModifier>(); //damage modifiers
 
-        public Health(int health) : this(health, health) {
-        }
+		public Health(int health) : this(health, health) {
+		}
 
-        public Health(int maxHealth, int currentHealth) {
-	        this.maxHealth = maxHealth;
-	        CurrentHealth = currentHealth;
-        }
+		public Health(int maxHealth, int currentHealth) {
+			this.maxHealth = maxHealth;
+			CurrentHealth = currentHealth;
+		}
 
-        public HealthChangeInfo SetHealth(int value, IInflictor inflictor) {
-	        int attemptedDelta = value - CurrentHealth;
+		public HealthChangeInfo SetHealth(int value, IInflictor inflictor) {
+			int attemptedDelta = value - CurrentHealth;
 			value = MathUtils.Clamp(value, 0, maxHealth);
 			int delta = value - CurrentHealth;
 
 			HealthChangeInfo info = new HealthChangeInfo() {
 				health = this,
 				oldHealth = CurrentHealth,
-                newHealth = value,
+				newHealth = value,
 				attemptedDelta = attemptedDelta,
-                inflictor = inflictor
+				inflictor = inflictor
 			};
 
 			CurrentHealth = value;
@@ -58,38 +58,38 @@ namespace RPGCuzWhyNot.Systems.HealthSystem {
 			}
 
 			return info;
-        }
+		}
 
-        public void RestoreAllHealth(IInflictor inflictor) {
-            SetHealth(maxHealth, inflictor);
-        }
+		public void RestoreAllHealth(IInflictor inflictor) {
+			SetHealth(maxHealth, inflictor);
+		}
 
-        public HealthChangeInfo TakeDamage(int damage, IInflictor inflictor) {
-            if (damage < 0) throw new ArgumentException("can't deal negative damage");
+		public HealthChangeInfo TakeDamage(int damage, IInflictor inflictor) {
+			if (damage < 0) throw new ArgumentException("can't deal negative damage");
 
-	        foreach (IHealthChangeModifier a in armor) {
-		        damage = a.OnDamageModify(damage);
-	        }
+			foreach (IHealthChangeModifier a in armor) {
+				damage = a.OnDamageModify(damage);
+			}
 
 			HealthChangeInfo info = ChangeHealth(-damage, inflictor);
 
-            return info;
-        }
+			return info;
+		}
 
-        public HealthChangeInfo Heal(int damage, IInflictor inflictor) {
-            if (damage < 0) throw new ArgumentException("can't heal negative damage");
+		public HealthChangeInfo Heal(int damage, IInflictor inflictor) {
+			if (damage < 0) throw new ArgumentException("can't heal negative damage");
 
-            foreach (IHealthChangeModifier a in armor) {
-	            damage = a.OnHealModify(damage);
-            }
+			foreach (IHealthChangeModifier a in armor) {
+				damage = a.OnHealModify(damage);
+			}
 
-	        HealthChangeInfo info = ChangeHealth(damage, inflictor);
-           
-            return info;
-        }
-		
+			HealthChangeInfo info = ChangeHealth(damage, inflictor);
+
+			return info;
+		}
+
 		protected HealthChangeInfo ChangeHealth(int damage, IInflictor inflictor) {
-            return SetHealth(CurrentHealth + damage, inflictor);
-        }
-    }
+			return SetHealth(CurrentHealth + damage, inflictor);
+		}
+	}
 }
--- RPGCuzWhyNot/Systems/HealthSystem/HealthChangeInfo.cs
+++ RPGCuzWhyNot/Systems/HealthSystem/HealthChangeInfo.cs
@@ -4,19 +4,19 @@ namespace RPGCuzWhyNot.Systems.HealthSystem {
 		public float oldHealth;
 		public float newHealth;
 		public float attemptedDelta;
-        public IInflictor inflictor;
-        public float Delta => newHealth - oldHealth;
+		public IInflictor inflictor;
+		public float Delta => newHealth - oldHealth;
 
 		public HealthChangeInfoSuccess Success {
-	        get {
-		        if (attemptedDelta != Delta) return HealthChangeInfoSuccess.PartialDelivery;
-		        if (inflictor?.Alignment != null && health.alignment != null && !inflictor.Alignment.CanHarm(health.alignment)) return HealthChangeInfoSuccess.AlignmentConflict; //can you hurt me?
-		        if (!health.IsAlive) return HealthChangeInfoSuccess.AlreadyDead; //is the health component dead?
-                return HealthChangeInfoSuccess.FullDelivery;
-	        }
-        }
+			get {
+				if (attemptedDelta != Delta) return HealthChangeInfoSuccess.PartialDelivery;
+				if (inflictor?.Alignment != null && health.alignment != null && !inflictor.Alignment.CanHarm(health.alignment)) return HealthChangeInfoSuccess.AlignmentConflict; //can you hurt me?
+				if (!health.IsAlive) return HealthChangeInfoSuccess.AlreadyDead; //is the health component dead?
+				return HealthChangeInfoSuccess.FullDelivery;
+			}
+		}
 
-        public override string ToString() {
+		public override string ToString() {
 			return $"old: {oldHealth}, new: {newHealth}, delta: {Delta}, attemptedDelta: {attemptedDelta}, Success:{Success}";
 		}
 	}
