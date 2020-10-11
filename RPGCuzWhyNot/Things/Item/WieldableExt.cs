178926d2151fb3794c7b3f126df8015f6ea55431
StaterZ <melvin@ringheim.se>
2020-10-11 18:35:40 +0200

    removed positive sign from requiredHands (this time properly)

--- RPGCuzWhyNot/Things/Item/WieldableExt.cs
+++ RPGCuzWhyNot/Things/Item/WieldableExt.cs
@@ -3,7 +3,7 @@ using RPGCuzWhyNot.Utilities;
 namespace RPGCuzWhyNot.Things.Item {
 	public static class WieldableExt {
 		public static string DefaultListingWithStats(IWieldable w) {
-			return $"{w.ListingName}  {Utils.FormatInt(w.HandsRequired)} hands required.";
+			return $"{w.ListingName}  {Utils.FormatInt(w.HandsRequired, false)} hands required.";
 		}
 	}
 }
