StaterZ/RPGCuzWhyNot

commit 434a380eb5f2e8c3a1334aac4d5dbb3e721d534e

Removed wieldable usage requirements and updated game data to work with new json serializer

RPGCuzWhyNot/Systems/Data/DataLoader.cs
@@ -24,10 +24,10 @@ namespace RPGCuzWhyNot.Systems.Data {
 
 		private static readonly JsonSerializer serializer = JsonSerializer.CreateDefault(serializerSettings);
 
-		private static Dictionary<string, Prototype> prototypes;
-		private static Dictionary<string, Location> locations;
-		private static Dictionary<string, NPC> npcs;
-		private static Dictionary<string, Type> npcTypeMap = new Dictionary<string, Type>();
+		private static readonly Dictionary<string, Prototype> prototypes = new Dictionary<string, Prototype>();
+		private static readonly Dictionary<string, Location> locations = new Dictionary<string, Location>();
+		private static readonly Dictionary<string, NPC> npcs = new Dictionary<string, NPC>();
+		private static readonly Dictionary<string, Type> npcTypeMap = new Dictionary<string, Type>();
 
 		private static bool loadError;
 
@@ -56,7 +56,6 @@ namespace RPGCuzWhyNot.Systems.Data {
 		public static bool LoadGameData() {
 			loadError = false;
 
-			prototypes = new Dictionary<string, Prototype>();
 			Prototypes = new ReadOnlyDictionary<string, Prototype>(prototypes);
 			LoadPrototypesFromPath<ItemPrototype>(itemsPath);
 			LoadPrototypesFromPath<LocationPrototype>(locationsPath);
@@ -186,7 +185,6 @@ namespace RPGCuzWhyNot.Systems.Data {
 		}
 
 		private static void ConstructLocations() {
-			locations = new Dictionary<string, Location>();
 			Locations = new ReadOnlyDictionary<string, Location>(locations);
 
 			// Create the locations.
@@ -197,7 +195,6 @@ namespace RPGCuzWhyNot.Systems.Data {
 		}
 
 		private static void ConstructNPCs() {
-			npcs = new Dictionary<string, NPC>();
 			NPCs = new ReadOnlyDictionary<string, NPC>(npcs);
 
 			// Create the NPCs.
@@ -279,23 +276,8 @@ namespace RPGCuzWhyNot.Systems.Data {
 		}
 
 		private static void ValidateItemPrototype(ItemPrototype proto) {
-			if (proto.DescriptionInInventory == null) MissingPropertyError(proto, "inventoryDescription");
-			if (proto.DescriptionOnGround == null) MissingPropertyError(proto, "groundDescription");
-
-			if (proto.IsWieldable) {
-				if (proto.HandsRequired == null) MissingPropertyError(proto, "handsRequired");
-			}
-
-			if (proto.IsWearable) {
-				if (proto.Defense == null) MissingPropertyError(proto, "defense");
-				if (proto.CoveredParts == 0) MissingPropertyError(proto, "coveredParts");
-				if (proto.CoveredLayers == 0) MissingPropertyError(proto, "coveredLayers");
-			}
-
-			if (proto.HasInventory) {
-				if (proto.WeightFraction.numerator == 0 && proto.WeightFraction.denominator == 0)
-					MissingPropertyError(proto, "weightFraction");
-			}
+			if (proto.Inventory != null && proto.Wieldable == null)
+				Error($"Item with inventory '{proto.Id}' must be wieldable, in file \"{proto.DataFilePath}\".");
 		}
 
 		private static void ValidateNpcPrototype(NpcPrototype proto) {

RPGCuzWhyNot/Systems/Data/Prototypes/ItemPrototype.cs
@@ -1,6 +1,7 @@
 using System;
 using Newtonsoft.Json;
 using RPGCuzWhyNot.Primitives;
+using RPGCuzWhyNot.Systems.Data.JsonConverters;
 using RPGCuzWhyNot.Things.Item;
 
 namespace RPGCuzWhyNot.Systems.Data.Prototypes {
@@ -18,37 +19,9 @@ namespace RPGCuzWhyNot.Systems.Data.Prototypes {
 		[JsonProperty("wearable")]
 		public WearableProps Wearable { get; set; }
 
-		[JsonPropertyName("handsRequired")]
-		public int? HandsRequired { get; set; }
-
-		[JsonPropertyName("requirements")]
-		public Requirements UsageRequirements { get; set; }
-
-		[JsonPropertyName("actions")]
+		[JsonProperty("actions")]
 		public ItemAction[] ItemActions { get; set; }
 
-		// Wearable
-
-		[JsonPropertyName("wearable")]
-		public bool IsWearable { get; set; }
-
-		[JsonPropertyName("defense")]
-		public int? Defense { get; set; }
-
-		[JsonPropertyName("coveredParts"), JsonConverter(typeof(JsonEnumConverter))]
-		public WearableSlots CoveredParts { get; set; }
-
-		[JsonPropertyName("coveredLayers"), JsonConverter(typeof(JsonEnumConverter))]
-		public WearableLayers CoveredLayers { get; set; }
-
-		// Inventory
-
-		[JsonPropertyName("hasInventory")]
-		public bool HasInventory { get; set; }
-
-		[JsonPropertyName("weightFraction"), JsonConverter(typeof(JsonFractionConverter))]
-		public Fraction WeightFraction { get; set; }
-		
 		[JsonProperty("inventory")]
 		public InventoryProps Inventory { get; set; }
 
@@ -80,8 +53,7 @@ namespace RPGCuzWhyNot.Systems.Data.Prototypes {
 
 			if (Wieldable != null) {
 				IWieldable wieldable = (IWieldable)item;
-				wieldable.HandsRequired = HandsRequired ?? 0;
-				wieldable.UsageRequirements = UsageRequirements ?? new Requirements();
+				wieldable.HandsRequired = Wieldable.HandsRequired;
 			}
 
 			if (Inventory != null) {
@@ -104,9 +76,6 @@ namespace RPGCuzWhyNot.Systems.Data.Prototypes {
 		public class WieldableProps {
 			[JsonProperty("handsRequired")]
 			public int HandsRequired { get; set; }
-
-			[JsonProperty("meleeDamage")]
-			public int MeleeDamage { get; set; }
 		}
 
 		[JsonObject(ItemRequired = Required.Always)]

RPGCuzWhyNot/Things/Item/IWieldable.cs
@@ -2,7 +2,6 @@ namespace RPGCuzWhyNot.Things.Item {
 	public interface IWieldable : IItem {
 		int HandsRequired { get; set; }
 		string ListingWithStats { get; }
-		Requirements UsageRequirements { get; set; }
 	}
 }
 

commit 5f07d8342d6e39e7c7572ed47a328835eb09c456

Fix change of behavior in DataLoader caused by commit `434a380` (StaterZ)

RPGCuzWhyNot/Systems/Data/DataLoader.cs
@@ -34,17 +34,17 @@ namespace RPGCuzWhyNot.Systems.Data {
 		/// <summary>
 		/// All of the loaded prototypes.
 		/// </summary>
-		public static ReadOnlyDictionary<string, Prototype> Prototypes { get; private set; }
+		public static ReadOnlyDictionary<string, Prototype> Prototypes { get; } = new ReadOnlyDictionary<string, Prototype>(prototypes);
 
 		/// <summary>
 		/// All of the loaded locations.
 		/// </summary>
-		public static ReadOnlyDictionary<string, Location> Locations { get; private set; }
+		public static ReadOnlyDictionary<string, Location> Locations { get; } = new ReadOnlyDictionary<string, Location>(locations);
 
 		/// <summary>
 		/// All of the loaded NPCs.
 		/// </summary>
-		public static ReadOnlyDictionary<string, NPC> NPCs { get; private set; }
+		public static ReadOnlyDictionary<string, NPC> NPCs { get; } = new ReadOnlyDictionary<string, NPC>(npcs);
 
 		static DataLoader() {
 			FindRegisteredNPCs();
@@ -56,7 +56,10 @@ namespace RPGCuzWhyNot.Systems.Data {
 		public static bool LoadGameData() {
 			loadError = false;
 
-			Prototypes = new ReadOnlyDictionary<string, Prototype>(prototypes);
+			prototypes.Clear();
+			locations.Clear();
+			npcs.Clear();
+
 			LoadPrototypesFromPath<ItemPrototype>(itemsPath);
 			LoadPrototypesFromPath<LocationPrototype>(locationsPath);
 			LoadPrototypesFromPath<NpcPrototype>(npcsPath);
@@ -185,8 +188,6 @@ namespace RPGCuzWhyNot.Systems.Data {
 		}
 
 		private static void ConstructLocations() {
-			Locations = new ReadOnlyDictionary<string, Location>(locations);
-
 			// Create the locations.
 			foreach (LocationPrototype prototype in prototypes.Values.OfType<LocationPrototype>()) {
 				Location location = prototype.Create();
@@ -195,8 +196,6 @@ namespace RPGCuzWhyNot.Systems.Data {
 		}
 
 		private static void ConstructNPCs() {
-			NPCs = new ReadOnlyDictionary<string, NPC>(npcs);
-
 			// Create the NPCs.
 			foreach (NpcPrototype proto in prototypes.Values.OfType<NpcPrototype>()) {
 				NPC npc = CreateNpc(proto);