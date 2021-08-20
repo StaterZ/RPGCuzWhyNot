8191fac4278a46aa85d7c0124b592561d7b711d4
StaterZ <57621744+StaterZ@users.noreply.github.com>
2021-08-20 16:18:55 +0200

    fix default npc referal bug

--- RPGCuzWhyNot/Things/Characters/NPCs/NPC.cs
+++ RPGCuzWhyNot/Things/Characters/NPCs/NPC.cs
@@ -5,7 +5,7 @@ using RPGCuzWhyNot.Things.Characters.Races.Humanoids;
 namespace RPGCuzWhyNot.Things.Characters.NPCs {
 	public abstract class NPC : Character, IConversable {
 		public virtual void Converse(Character character, string response) {
-			Terminal.WriteLine($"No answer, it appears {Referral} mute...");
+			Terminal.WriteLine($"No answer, it appears {Referral.objectPronoun}'s mute...");
 		}
 
 		protected NPC(Race race) : base(race) {
