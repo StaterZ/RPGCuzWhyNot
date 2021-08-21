a2442146ce18a933ed9a885868b4a22bb2a71ed9
StaterZ <57621744+StaterZ@users.noreply.github.com>
2021-08-21 16:44:42 +0200

    fix incorrect npc referral

--- RPGCuzWhyNot/Things/Characters/NPCs/NPC.cs
+++ RPGCuzWhyNot/Things/Characters/NPCs/NPC.cs
@@ -4,7 +4,7 @@ using RPGCuzWhyNot.Things.Characters.Races;
 namespace RPGCuzWhyNot.Things.Characters.NPCs {
 	public abstract class NPC : Character, IConversable {
 		public virtual void Converse(Character character, string response) {
-			Terminal.WriteLine($"No answer, it appears {Referral.objectPronoun}'s mute...");
+			Terminal.WriteLine($"No answer, it appears {Referral.subjectPronoun}'s mute...");
 		}
 
 		protected NPC(Race race) : base(race) {
