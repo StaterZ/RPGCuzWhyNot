749d75e3d7175b366d37893d8f74229cb5f1233e
StaterZ <57621744+StaterZ@users.noreply.github.com>
2022-10-30 00:12:43 +0200

    add short version of inventory command

--- RPGCuzWhyNot/Systems/CommandSystem/Commands/InventoryCommand.cs
+++ RPGCuzWhyNot/Systems/CommandSystem/Commands/InventoryCommand.cs
@@ -2,7 +2,7 @@ using RPGCuzWhyNot.Things;
 
 namespace RPGCuzWhyNot.Systems.CommandSystem.Commands {
 	public class InventoryCommand : Command {
-		public override string[] CallNames { get; } = {"inventory"};
+		public override string[] CallNames { get; } = {"inv", "inventory"};
 		public override string HelpText { get; } = "List the items in your inventory";
 
 		public override void Execute(CommandArguments args) {
